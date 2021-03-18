25e43903907604f28efbf0eb8365b9ff462aa889
luka chemia <luka.chemia001@gmailcom>
2021-03-18 13:13:43 +0400

    kodi

---
+++ pythonII..py
@@ -0,0 +1,58 @@
+#Task1
+
+# class Cal():
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#     def dama(self):
+#         return self.a +self.b
+#     def gamo(self):
+#         return self.a -self.b
+#     def gamr(self):
+#         return self.a *self.b
+#     def gayo(self):
+#         return self.a /self.b
+# a = int(input("first num: "))
+# b = int(input("second num: "))
+# obj = Cal(a,b)
+# choice = 1
+# while choice !=0:
+#     print("0.Exit")
+#     print("1.damateba")
+#     print("2.gamokleba")
+#     print("3.gamravleba")
+#     print("4.gayofa")
+#
+#     choice = int(input("enter choice: "))
+#     if choice == 1:
+#         print("result =  ", obj.dama())
+#     elif choice == 2:
+#         print("result =  ", obj.gamo())
+#     elif choice == 3:
+#         print("result =  ", obj.gamr())
+#     elif choice == 4:
+#         print("result =  ",round (obj.gayo(), 2))
+#     elif choice == 0:
+#         print("exiting")
+#     else:
+#         print("invalid choice!")
+# print()
+# Task2
+#
+# class Rectangle():
+#
+#     def __init__(self, width,lenght):
+#         self.width= width
+#         self.lenght = lenght
+#     def area(self):
+#         return self.width * self.lenght
+#     def perimeter(self):
+#         return ((2 * int(self.width)) + (2 * int(self.lenght)))
+#     def print_info(self):
+#         return  f"width = {self.width}, lenght = {self.lenght}, Area = {self.area()}, perimeter = {self.perimeter()}."
+#
+#
+# r = Rectangle(3,4)
+# print(r.print_info())
+
+#Task3
